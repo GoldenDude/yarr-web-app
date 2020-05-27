24c3820f13a2c398bed8bb1d0664aa3e7cdac047
GoldenDude <edanazran@gmail.com>
2020-05-27 18:29:47 +0300

    Updates

---
+++ precache-manifest.4288899f6e6d121863d5b8dbea262416.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44cedc94b143f0f97aa5e06fdc1639d7",
+    "url": "/yarr-web-app/index.html"
+  },
+  {
+    "revision": "621db43b674fc91041e8",
+    "url": "/yarr-web-app/static/css/2.e5fc3193.chunk.css"
+  },
+  {
+    "revision": "604c5de7a3174bb98363",
+    "url": "/yarr-web-app/static/css/main.3b0746e0.chunk.css"
+  },
+  {
+    "revision": "621db43b674fc91041e8",
+    "url": "/yarr-web-app/static/js/2.9bcf2a1d.chunk.js"
+  },
+  {
+    "revision": "3a9f612b5795358919f90b4f2504d841",
+    "url": "/yarr-web-app/static/js/2.9bcf2a1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "604c5de7a3174bb98363",
+    "url": "/yarr-web-app/static/js/main.6080b3d7.chunk.js"
+  },
+  {
+    "revision": "6ecd74ff0b3b578bdcca",
+    "url": "/yarr-web-app/static/js/runtime-main.3529593f.js"
+  },
+  {
+    "revision": "23ca55b383be9fe98f183e3c7909efe5",
+    "url": "/yarr-web-app/static/media/Game.23ca55b3.png"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/yarr-web-app/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/yarr-web-app/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "de97ae4c59ace483e8ebd107eb655b7b",
+    "url": "/yarr-web-app/static/media/blue_yellow_color_blindness.de97ae4c.jpg"
+  },
+  {
+    "revision": "0ae8f2c13e414eff20292116bd02d133",
+    "url": "/yarr-web-app/static/media/competitive.0ae8f2c1.png"
+  },
+  {
+    "revision": "9cc01480e71afa0e2f4892f2ebddc1a7",
+    "url": "/yarr-web-app/static/media/cooperative.9cc01480.png"
+  },
+  {
+    "revision": "5057b7825604800ae400906932e19f6f",
+    "url": "/yarr-web-app/static/media/different_colors.5057b782.png"
+  },
+  {
+    "revision": "fb42c3f442f5ab6c128651a338172657",
+    "url": "/yarr-web-app/static/media/different_design.fb42c3f4.png"
+  },
+  {
+    "revision": "aa64bef990035a701d6dbf09fad1b9d2",
+    "url": "/yarr-web-app/static/media/different_shapes.aa64bef9.png"
+  },
+  {
+    "revision": "067595ad77ecc0db9c81c8905a7eef32",
+    "url": "/yarr-web-app/static/media/fa-brands-400.067595ad.woff2"
+  },
+  {
+    "revision": "57dcda6f368ea90179f75cbdae96c263",
+    "url": "/yarr-web-app/static/media/fa-brands-400.57dcda6f.eot"
+  },
+  {
+    "revision": "9d67fa1429375bd2a899a17eb77d0342",
+    "url": "/yarr-web-app/static/media/fa-brands-400.9d67fa14.svg"
+  },
+  {
+    "revision": "9ec698d1a597bff5df337094b71ddaaf",
+    "url": "/yarr-web-app/static/media/fa-brands-400.9ec698d1.ttf"
+  },
+  {
+    "revision": "b564da88bbf0c4aa446fa19653713cd1",
+    "url": "/yarr-web-app/static/media/fa-brands-400.b564da88.woff"
+  },
+  {
+    "revision": "3351f435b3c9037fd88aeb04dc1e43bc",
+    "url": "/yarr-web-app/static/media/fa-regular-400.3351f435.eot"
+  },
+  {
+    "revision": "4165c2688309cbfb1b877caf8f75afb5",
+    "url": "/yarr-web-app/static/media/fa-regular-400.4165c268.woff2"
+  },
+  {
+    "revision": "5d0861781aeef6c82fda3a3076954a1b",
+    "url": "/yarr-web-app/static/media/fa-regular-400.5d086178.svg"
+  },
+  {
+    "revision": "73cf49a2232c06c920b7a34e36bfb58c",
+    "url": "/yarr-web-app/static/media/fa-regular-400.73cf49a2.woff"
+  },
+  {
+    "revision": "a0e3ac82940c1998c5977fd4bc1f5ef6",
+    "url": "/yarr-web-app/static/media/fa-regular-400.a0e3ac82.ttf"
+  },
+  {
+    "revision": "0724bb8b89ab6b8b9b7df917b17be0b7",
+    "url": "/yarr-web-app/static/media/fa-solid-900.0724bb8b.svg"
+  },
+  {
+    "revision": "55eb2a60e8181f0e68b558c991973bf0",
+    "url": "/yarr-web-app/static/media/fa-solid-900.55eb2a60.woff2"
+  },
+  {
+    "revision": "75f38a159982b6bd1704891332d95fa7",
+    "url": "/yarr-web-app/static/media/fa-solid-900.75f38a15.ttf"
+  },
+  {
+    "revision": "89e02bae13c9131c7468b1e729339ac1",
+    "url": "/yarr-web-app/static/media/fa-solid-900.89e02bae.eot"
+  },
+  {
+    "revision": "cdfec5cf5e9840889790bcf2c4042583",
+    "url": "/yarr-web-app/static/media/fa-solid-900.cdfec5cf.woff"
+  },
+  {
+    "revision": "b6b770bfcf0c725aa716aa0b2d8a50a0",
+    "url": "/yarr-web-app/static/media/full_spectrum_vision.b6b770bf.jpg"
+  },
+  {
+    "revision": "2490b99e58d823fb5cc70a8280d292be",
+    "url": "/yarr-web-app/static/media/red_green_color_blindness.2490b99e.jpg"
+  }
+]);
