713467eab550f65312e2f584293d88c319a23f1d
GoldenDude <edanazran@gmail.com>
2020-06-01 08:54:23 +0300

    edan

--- src/Reducers/rootReducer.js
+++ src/Reducers/rootReducer.js
@@ -3,10 +3,21 @@ import UserReducer from './UserReducer'
 import StudyReducer from './StudyReducer'
 import ExperimentReducer from './ExperimentReducer'
 import BreadcrumbsReducer from './BreadcrumbsReducer'
+import { USER_LOGOUT } from '../ActionsTypes/UserActionTypes'
 
-export default combineReducers({
+const appReducer = combineReducers({
   user: UserReducer,
   study: StudyReducer,
   experiment: ExperimentReducer,
   breadcrumbs: BreadcrumbsReducer
 })
+
+const rootReducer = (state, action) => {
+  if (action.type === USER_LOGOUT) {
+    state = undefined
+  }
+
+  return appReducer(state, action)
+}
+
+export default rootReducer 
